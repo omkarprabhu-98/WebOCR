eb75a8996b655971d89198af2124ca10ea98c094
Omkar Prabhu <omkkarprabuinfinity@gmail.com>
2017-09-01 10:51:04 +0530

    image contrast

--- routes/ocr.js
+++ routes/ocr.js
@@ -5,12 +5,13 @@ var express = require('express');
 var router = express.Router();
 var multer  = require('multer');
 var upload = multer({ dest: 'uploads/' });
+var Jimp = require("jimp");
 
 var tesseract = require('./../libs/node-tesseract');
 
 
-
 router.post('/', upload.single('file'), function(req, res) {
+
 
     // specify location of tesseract
     var options = {
@@ -19,16 +20,31 @@ router.post('/', upload.single('file'), function(req, res) {
         config: '--tessdata "./libs/tesseract-ocr/tessdata"'
     };
 
-    // process image
-    tesseract.process('./uploads/'+ req.file.filename, options, function(err, text){
-        if(err){
-            return console.log("An error occured: ", err);
-        }
+    Jimp.read('./uploads/'+ req.file.filename, function (err, lenna) {
 
-        console.log("Recognized text:");
-        console.log(text);
-        res.send(text);
+        var file = "res." + lenna.getExtension();
+
+        if (err) throw err;
+        lenna.quality(100)                 // set JPEG quality
+            .contrast(1)
+            .greyscale()
+            .write(file); // save
+
+
+
+        // process image
+        tesseract.process(file, options, function(err, text){
+            if(err){
+                return console.log("An error occured: ", err);
+            }
+
+            console.log("Recognized text:");
+            console.log(text);
+            res.send(text);
+        });
+
     });
+
 
 });
 
